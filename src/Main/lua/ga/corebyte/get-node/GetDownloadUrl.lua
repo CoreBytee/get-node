947e3c2d494df9afb283738c3f4ae1951f04b1ec
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-07 20:31:34 +0200

    Update GetDownloadUrl.lua

--- src/Main/lua/ga/corebyte/get-node/GetDownloadUrl.lua
+++ src/Main/lua/ga/corebyte/get-node/GetDownloadUrl.lua
@@ -1,5 +1,5 @@
 local Request = require("coro-http").request
-local BaseUrl = "https://nodejs.org/dist/latest/"
+local BaseUrl = "https://nodejs.org/dist/"
 
 local Names =  {
     ["win32"] = "node-%s-win-%s.zip",
@@ -7,22 +7,13 @@ local Names =  {
     ["darwin"] = "node-%s-darwin-%s.tar.gz"
 }
 
-local function GetVersion()
-    local Request, Body = Request(
-        "GET",
-        BaseUrl .. "SHASUMS256.txt"
-    )
-    local Version = Body:split("\n")[1]:split("  ")[2]:split("-")[2]
-    return Version
-end
-
-return function ()
+return function (Version)
     if Names[TypeWriter.Os] == nil then
         return false
     end
-    return BaseUrl .. string.format(
+    return BaseUrl .. Version .. "/" .. string.format(
         Names[TypeWriter.Os],
-        GetVersion(),
+        Version,
         require("jit").arch
     )
 end
